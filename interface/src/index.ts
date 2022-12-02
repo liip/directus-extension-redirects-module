aa7938f1a673cc2831bf53f120399ba460e08c80
Leo Roettele <leo.roettele@liip.ch>
2022-12-02 17:51:21 +0100

    WIP

--- interface/src/index.ts
+++ interface/src/index.ts
@@ -2,11 +2,25 @@ import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
-	id: 'custom',
-	name: 'Custom',
+	id: 'redirectionURL',
+	name: 'Redirection-URL',
 	icon: 'box',
 	description: 'This is my custom interface!',
 	component: InterfaceComponent,
-	options: null,
+	group: 'standard',
 	types: ['string'],
+	options: [
+		{
+			field: 'allowExternalLinks',
+			name: 'Allow external Links',
+			type: 'boolean',
+			meta: {
+				width: 'half',
+				interface: 'boolean',
+			},
+			schema: {
+				default_value: false,
+			},
+		},
+	],
 });
--- interface/src/interface.vue
+++ interface/src/interface.vue
@@ -1,24 +1,65 @@
 <template>
-	<input :value="value" @input="handleChange($event.target.value)" />
+  <div>
+    <v-input
+        :model-value="value"
+        @update:model-value="onChange"
+    />
+    {{validateInput(value, fieldsData.regex)}}
+  </div>
 </template>
+<script setup lang="ts">
+import { defineProps, defineEmits, ref, computed } from 'vue';
+import { useApi } from '@directus/extensions-sdk';
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+const props = defineProps({
+  value: {
+    type: String,
+    default: '',
+  },
+  allowExternalLinks: {
+    type: Boolean,
+    default: false,
+  },
+  collection: {
+    type: String,
+    required: true,
+  },
+  field: {
+    type: String,
+    required: true,
+  },
+  primaryKey: {
+    type: Number
+  }
+})
 
-export default defineComponent({
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
-	},
-	emits: ['input'],
-	setup(props, { emit }) {
-		return { handleChange };
+const emit = defineEmits(['input'])
+const fieldsData = computed(async () => {
+  const api = useApi()
+  const response = await api.get(`/items/${props.collection}?filter[id][_eq]=${props.primaryKey}`);
+  return response.data.data;
+})
 
-		function handleChange(value: string): void {
-			emit('input', value);
-		}
-	},
-});
+const validateInput = (input: string, isRegex: boolean) => {
+  const errors: string[] = []
+  if (isRegex){
+    // TODO: Validate Regex
+  } else {
+    if (!input.startsWith('/')){
+      console.log(1)
+      errors.push('must start with a /')
+    } else if (input.includes(' ')){
+      console.log(2)
+      errors.push('can\'t contain spaces')
+    }
+  }
+  return errors;
+}
+
+const onChange = (value: string) => {
+  if (props.value === value) return;
+
+  // Emit exact value.
+  emit('input', value || '');
+}
 </script>
