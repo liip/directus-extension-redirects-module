4019e0675a9968b182c193f20270dc215202bc63
Leo Roettele <leo.roettele@liip.ch>
2022-12-07 15:43:01 +0100

    match strings on database and regex in ts

--- endpoint/src/index.ts
+++ endpoint/src/index.ts
@@ -2,24 +2,38 @@ import { defineEndpoint } from '@directus/extensions-sdk';
 import { RedirectItem } from './types';
 
 export default defineEndpoint((router, { services }) => {
-	const { ItemsService } = services;
+	const {ItemsService} = services;
 
 	router.get('/find', async (req, res) => {
 
 		const redirectsService = new ItemsService(
 			'redirects',
-			{ schema: req.schema, accountability: req.accountability }
+			{schema: req.schema, accountability: req.accountability}
 		);
 
-		const redirects: RedirectItem[] =
-			await redirectsService.readByQuery({ fields: ['*'] });
+		const match = await redirectsService.readByQuery({
+			fields: ['*'],
+			filter: {
+				regex: {_eq: false},
+				from: {_eq: req.query.path},
+			},
+		});
 
-		const response: RedirectItem[] =
-			redirects.filter((redirectItem) => {
-				const regex: RegExp = new RegExp(redirectItem.from);
-        return redirectItem.regex ? regex.test(req.query.path) : redirectItem.from === req.query.path;
-      });
+		if (match.length) {
+			res.send(match[0]?.to);
+		} else {
+			const redirectsWithRegex: RedirectItem[] = await redirectsService.readByQuery({
+				fields: ['*'],
+				filter: {regex: {_eq: true}}
+			});
 
-		res.send(response[0]?.to);
+			const response: RedirectItem[] =
+				redirectsWithRegex.filter((redirectItem) => {
+					const regex: RegExp = new RegExp(redirectItem.from);
+					return redirectItem.regex ? regex.test(req.query.path) : redirectItem.from === req.query.path;
+				});
+
+			res.send(response[0]?.to);
+		}
 	});
 });
